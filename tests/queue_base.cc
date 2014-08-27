3b50ea562b73b38074312a6e0b5161fcea25f0a1
Alessio Guerrieri <Pufforrohk@gmail.com>
2014-08-27 13:53:08 +0200

    Implement burst-based test of safe queue

---
+++ tests/queue_base.cc
@@ -0,0 +1,38 @@
+#include "../src/safequeue.hh"
+#include <iostream>
+#include <thread>
+#include <cassert>
+using namespace utils;
+
+//Waiting time between two bursts of pushes
+#define PRODUCER_STEP 100
+//Number of bursts
+#define PRODUCER_ITERATIONS 15
+//Number of pushes per burst
+#define PRODUCER_RATE 10000
+
+int main(int argc, char** argv){
+  safequeue<int> q;
+
+  std::thread producer([&q](){
+      int counter = 0;
+      for(int it = 0; it<PRODUCER_ITERATIONS ; it++){
+        std::this_thread::sleep_for(std::chrono::milliseconds(PRODUCER_STEP));
+        for(int i=0;i<PRODUCER_RATE;i++,counter++)
+          q.push(counter);
+      }
+    });
+
+  std::thread consumer([&q](){
+      int cur = -1;
+      while(cur+1<PRODUCER_ITERATIONS * PRODUCER_RATE){
+        int el=q.pop();
+        assert(el==cur+1);
+        cur=el;
+        std::cout<<el<<std::endl;
+      }
+    });
+  producer.join();
+  consumer.join();
+  return 0;
+}
