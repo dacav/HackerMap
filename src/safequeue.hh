15c5c40abbf45e45e83b88e59c28473821baa4eb
Giovanni Simoni <simgidacav@gmail.com>
2014-08-30 15:39:59 +0200

    Queues now allow termination.

--- src/safequeue.hh
+++ src/safequeue.hh
@@ -3,13 +3,20 @@
 #include <queue>
 #include <thread>
 #include <mutex>
+#include <atomic>
+#include <exception>
 #include <condition_variable>
 
 namespace utils {
+
+    class Terminated: public std::exception {
+        public:
+            Terminated() : std::exception() {}
+    };
+
   // The implementation of this class must be in the header because templates
   template <typename T> 
-  class safequeue
-  {
+  class SafeQueue {
   public:
     // Pushes an item into the queue and notify one of the waiting threads
     void push(const T& item){
@@ -19,22 +26,38 @@ namespace utils {
       waiting.notify_one();
     }
 
+    void terminate() {
+        running = false;
+        waiting.notify_all();
+    }
+
     //Waits until there is something in the queue and pops it
     //The lock is managed by the the condition variable
     T pop(){
       std::unique_lock<std::mutex> lock(mutex);
-      while(queue.empty()){
+      while(queue.empty() && running){
         waiting.wait(lock);
-      }      
+      }
+
+      if (not running) {
+          throw Terminated();
+      }
       T t = queue.front();
       queue.pop();
       return t;
+    }
+
+    SafeQueue()
+      : running(true)
+    {
     }
 
   private:
     std::queue<T> queue;
     std::mutex mutex;
     std::condition_variable waiting;
+
+    std::atomic<bool> running;
   };
 
 }
--- tests/queue_base.cc
+++ tests/queue_base.cc
@@ -1,4 +1,4 @@
-#include "../src/safequeue.hh"
+#include <safequeue.hh>
 #include <iostream>
 #include <thread>
 #include <cassert>
@@ -16,28 +16,37 @@ Right now this test does not take any parameter
 Is it possible to run this test automatically with different parameters?
  */
 int main(int argc, char** argv){
-  safequeue<int> q;
+    SafeQueue<int> q;
+    bool good = false;
 
-  std::thread producer([&q](){
-      int counter = 0;
-      for(int it = 0; it<PRODUCER_ITERATIONS ; it++){
-        std::this_thread::sleep_for(std::chrono::milliseconds(PRODUCER_STEP));
-        for(int i=0;i<PRODUCER_RATE;i++,counter++)
-          q.push(counter);
-      }
+    std::thread producer([&q](){
+        int counter = 0;
+        for(int it = 0; it<PRODUCER_ITERATIONS ; it++){
+            std::this_thread::sleep_for(std::chrono::milliseconds(PRODUCER_STEP));
+            for(int i=0;i<PRODUCER_RATE;i++,counter++) {
+                q.push(counter);
+            }
+        }
     });
 
-  std::thread consumer([&q](){
-      int cur = -1;
-      while(cur+1<PRODUCER_ITERATIONS * PRODUCER_RATE){
-        int el=q.pop();
-        //check if we are popping in the right order
-        assert(el==cur+1); 
-        cur=el;
-        std::cout<<el<<std::endl;
-      }
+    std::thread consumer([&q, &good](){
+        int cur = -1;
+        while (not good) {
+            try {
+                int el=q.pop();
+                //check if we are popping in the right order
+                assert(el==cur+1);
+                cur=el;
+                std::cout<<el<<std::endl;
+            } catch (Terminated &t) {
+                good = true;
+            }
+        }
     });
-  producer.join();
-  consumer.join();
-  return 0;
+
+    producer.join();
+    q.terminate();
+    consumer.join();
+    assert(good);
+    return 0;
 }
