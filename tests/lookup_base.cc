10abdbe01ae5264eb46d37b2f1d616052ab28bcf
Alessio Guerrieri <Pufforrohk@gmail.com>
2014-08-30 16:14:29 +0200

    Threaded version of lookup

--- tests/lookup_base.cc
+++ tests/lookup_base.cc
@@ -1,11 +1,24 @@
 #include <lookup.hh>
 
 #include <iostream>
+#include <thread>
+#include <safequeue.hh>
+using namespace std;
 
 int main (int argc, char **argv)
 {
-    auto coords = lookup::GeoIp("geoip.example.com").lookup("oglaf.com");
-    std::cout << coords.longitude << ", " << coords.latitude << std::endl;
-
-    return 0;
+  lookup::GeoIp geo("geoip.example.com");
+  utils::SafeQueue<lookup::Coords> queue;
+  geo.set_output(&queue);
+  geo.run();
+  for(int i=0;i<100;i++){
+    geo.messages.push("www.google.it");    
+  }
+  for(int i=0;i<100;i++){
+    auto coords = queue.pop();
+    cout << coords.longitude << ", " << coords.latitude << endl;
+  }
+  geo.terminate();
+  geo.join();
+  return 0;
 }
