dc582ba84bc7616b10d63fef7580344d1029fc25
Giovanni Simoni <simgidacav@gmail.com>
2014-08-30 20:42:45 +0200

    Magic script for code-formatting

--- scripts/fix_style.sh
+++ scripts/fix_style.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-if [ -z $1 ]; then
-  FILES=`git diff --cached --name-only | egrep '\.(cc|hh)$'`
-else
-  FILES=$@
-fi
-
 ASTYLE='astyle --suffix=none --style=knf --indent=spaces=4 --indent-namespaces --indent-modifiers'
-for fn in $FILES; do
-  echo "Fixing style for ${fn}"
-  $ASTYLE $fn
-done
+if [ -z $1 ]; then
+  for fn in `git diff --cached --name-only | egrep '\.(cc|hh)$'`; do
+    save="$(dirname ${fn})/.$(basename ${fn}).non-staged"
+    git diff $fn > $save
+    git co $fn
+    $ASTYLE $fn
+    git add $fn
+    git apply < $save
+  done
+else
+  for fn in $@; do
+    echo "Fixing style for ${fn}"
+    $ASTYLE $fn
+  done
+fi
