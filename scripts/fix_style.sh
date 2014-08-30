ca8ffa5699a8516c2d54b311e131cafc4e5bc659
Giovanni Simoni <simgidacav@gmail.com>
2014-08-30 18:45:29 +0200

    Script fix 2

--- scripts/fix_style.sh
+++ scripts/fix_style.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+if [ -z $1 ]; then
+  FILES=`git diff --cached --name-only | egrep '\.(cc|hh)$'`
+else
+  FILES=$@
+fi
+
 ASTYLE='astyle --suffix=none --style=knf --indent=spaces=4 --indent-namespaces --indent-modifiers'
-for fn in `git diff --cached --name-only | egrep '\.(cc|hh)$'`; do
+for fn in $FILES; do
   echo "Fixing style for ${fn}"
   $ASTYLE $fn
 done
